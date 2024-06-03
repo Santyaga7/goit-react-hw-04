c669aa621bb022c08cce8e6a9e87a08fea3794b8
Santyaga7 <149270626+Santyaga7@users.noreply.github.com>
2024-06-03 10:20:32 +0300

    Update ImageCard.jsx

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -4,11 +4,10 @@ const ImageCard = ({ alt_description, urls, updateModalStateData }) => {
 	return (
 		<div>
 			<img
-				className={styles.cardWrapper}
-			        onClick={() => updateModalStateData(urls.regular, alt_description)}
-				className={styles.cardImage}
-				src={urls.small}
-				alt={alt_description}
+    				className={`${styles.cardWrapper} ${styles.cardImage}`}
+    				onClick={() => updateModalStateData(urls.regular, alt_description)}
+    				src={urls.small}
+    				alt={alt_description}
 			/>
 			<p className={styles.cardDescription}>{alt_description}</p>
 		</div>
