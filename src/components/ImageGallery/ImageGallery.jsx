e8fe0f9266a1d1025f246cf57427b3957126996b
Santyaga7 <149270626+Santyaga7@users.noreply.github.com>
2024-06-03 21:51:55 +0300

    Update ImageGallery.jsx
    info to function "openModal"

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,10 +2,14 @@ import ImageCard from '../ImageCard/ImageCard';
 import styles from './ImageGallery.module.css';
 
 const ImageGallery = ({ gallery, openModal, updateModalStateData }) => {
+	const handleImageClick = (imageData) => {
+		openModal(imageData);
+	};
+
 	return (
 		<ul className={styles.itemsContainer}>
 			{gallery.map(({ id, alt_description, urls }) => (
-				<li className={styles.cardItem} key={id} onClick={openModal}>
+				<li className={styles.cardItem} key={id} onClick={() => handleImageClick({ id, alt_description, urls })}>
 					<ImageCard
 						urls={urls}
 						alt_description={alt_description}
